6a1639eeafd844d3d10e18dd367a9cd5a91a6985
Manoj Patel <nycdays300@gmail.com>
2020-01-02 01:39:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -35,4 +35,8 @@ function logShout(string) {
    string = uppercase
    console.log (uppercase)
 }
-
+function logShout(string) {
+   uppercase=string.toUpperCase();
+   string=uppercase;
+   console.log (uppercase);
+   
